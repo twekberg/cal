ca5ad0ff8e4891522065eb2009c63f120102de53
twekberg <tekberg@gmail.com>
2025-06-19 18:24:29 -0700

    The program works now.

--- README.md
+++ README.md
@@ -1,28 +1,44 @@
 # cal
-Draw a month for a wall calendar.
 
-Use this a basis to print 12 months of a wall calendar.
+# Introduction
 
-Uses a venv that contains PIL.
+Draws a wall calendar. The title page (first) shows the year and a
+title. The rest of the pages show image, month calendar pages for all
+12 months. The images are listed in a JSON file with these keys::
 
-Renders the page to the default program that displays PNG files.
-I have that set to use gimp.
+    - directory - fully specified directory containing the images.
+	- title_text - text to show in the title page.
+	- images: a list of image file names in the directory. There should be 12
+	- images. Any extra are ignored.
 
-Usage:
+# Usage
 
- ./pil_calendar -h
- usage: pil_calendar.py [-h] [-a {calendar,demo}] [-y YEAR] [-m MONTH]
+    $ ./make_calendar.py -h
+    usage: make_calendar.py [-h] [-i INPUT_FILENAME] [-y YEAR] [-q]
 
-Example of PIL rendering. It renders to a file, when shown is brought
-up with Gimp.
+    Complete program that makes a wall calendar with images.
 
-The advantage of using PIL is that a LARGE font can be used (100 pts).
-tkinter can only support fonts up to a maximum of 30 pts.
+    options:
+      -h, --help            show this help message and exit
+      -i INPUT_FILENAME, --input_filename INPUT_FILENAME
+                            JSON file with image data. Default: rachel_welch.json.
+      -y YEAR, --year YEAR  The year to draw. default: 2025
+      -q, --quiet           Don't show the images. Only create JPG files. default:
+                            False
+Uses a venv that contains PIL for rendering the graphics. I happen to
+have a venv that contains PIL so I use that.
 
-options:
-  -h, --help            show this help message and exit
-  -a {calendar,demo}, --action {calendar,demo}
-                        Type of action to perform. default: calendar
-  -y YEAR, --year YEAR  The year to draw. default: 2025
-  -m MONTH, --month MONTH
-                        The month to draw. default: 6
+The output for the calendar is year.pdf, whatever year is
+specified. Use Adobe Acrobat to display and print the wall calendar.
+
+# TODO
+
+  - The images are scaled and centered on the page so they print
+    correctly. Because of this, the current directory contains 12 JPG
+    files for the scalled images. In addition, there is a JPG file for
+    each month and title.jpg for the title page. There should be
+    cleanup to remove these files, as either using a --cleanup
+    parameter or doing that after creating the PDF file.
+  - Think about adding a feature to specially note holidays. (Since I'm
+    retired, every day is a holiday for me.)
+  - Create a venv for this program and document it here.
---
+++ make_calendar.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python
+"""
+Complete program that makes a wall calendar with images.
+"""
+
+
+import argparse
+import calendar
+from datetime import datetime
+import itertools
+import json
+from pathlib import Path
+from PIL import Image, ImageDraw, ImageFont
+import subprocess
+import sys
+import tkinter as tk
+
+
+# Static data items are stored here.
+DATA = {"huge_font":  ImageFont.truetype('times.ttf', 300),
+        "title_font": ImageFont.truetype('times.ttf', 80),
+        "base_font":  ImageFont.truetype('times.ttf', 50),
+        "small_font": ImageFont.truetype('times.ttf', 30),
+        "box_width": 146,
+        "box_height": 90,
+        "root_width": 1050,
+        "root_height": 800,
+        }
+
+
+def build_parser():
+    """
+    Collect command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-i', '--input_filename',
+                        default='rachel_welch.json',
+                        help='JSON file with image data. '
+                        ' Default: %(default)s.')
+    parser.add_argument('-y', '--year', default=datetime.now().year, type=int,
+                        help='The year to draw. '
+                        'default: %(default)s')
+    parser.add_argument('-q', '--quiet',
+                        default=False, action='store_true',
+                        help="Don't show the images. Only create JPG files. "
+                        'default: %(default)s')
+    return parser
+
+
+class RenderMonth():
+    """
+    Render an image for a specific month.
+    """
+    def __init__(self, args, year, month):
+        self.args = args
+        self.im = Image.new('RGB', (DATA['root_width'], DATA['root_height']), (256, 256, 256))
+        self.draw = ImageDraw.Draw(self.im)
+        cal = calendar.Calendar(firstweekday=list(calendar.day_name).index('Sunday'))
+        self.month_name = calendar.month_name[month]
+        if not self.args.quiet:
+            print(self.month_name)
+        self.month_cal = cal.monthdays2calendar(year, month)
+        if not self.args.quiet:
+            for week in self.month_cal:
+                print(week)
+
+    def save(self):
+        """
+        Save the image as a JPG file.
+        """
+        self.im.save(f'{self.month_name}.jpg', "JPEG", optimize=True, quality=80)
+
+
+    def show(self):
+        """
+        Show the image in gimp. This is for use in the debugger.
+        """
+        if not self.args.quiet:
+            self.im.show()
+
+
+    def draw_calendar(self):
+        """
+        Draw the calendar
+        """
+        self.draw_lines()
+        self.draw_month()
+        self.draw_dates()
+        self.draw_days_of_week()
+
+
+    def draw_days_of_week(self):
+        """
+        Draw the days of the week column headers.
+        """
+        y_coord = 115
+        x_coord = 10
+        W, H = (DATA['box_width'], 100)
+        for (_, dow_number) in self.month_cal[0]:
+            day_name = calendar.day_name[dow_number]
+            _, _, w, h = self.draw.textbbox((0, 0), day_name, font=DATA["small_font"])
+            self.draw.text((x_coord + (W-w)/2, y_coord), day_name, font=DATA['small_font'], fill='green')
+            x_coord += DATA['box_width']
+            
+
+    def draw_dates(self):
+        """
+        Draw the numbered dates.
+        """
+        y_coord = 170
+        for week in self.month_cal:
+            # Ignore DOW
+            x_coord = 30
+            for (day, _) in week:
+                if day:
+                    self.draw.text((x_coord, y_coord), f'{day}', font=DATA['base_font'], fill='red')
+                x_coord += DATA['box_width']
+            y_coord += DATA['box_height']
+
+    def draw_month(self):
+        """
+        Draw the month name at the top.
+        """
+        W, H = (self.im.width, 100)
+        _, _, w, h = self.draw.textbbox((0, 0), self.month_name, font=DATA['title_font'])
+        self.draw.text(((W-w)/2, (H-h)/2), self.month_name, font=DATA['title_font'], fill='blue')
+
+
+    def draw_lines(self):
+        """
+        Draw the vertical and horizontal lines.
+        """
+        def calc_y_bottom():
+            """
+            Months have 4, 5, or 6 weeks. This calculation is done to
+            have the bottom of the vertical lines line up properly.
+            2015/2 has 4 weeks, 2025/6 has 5 weeks, 2025/3 has 6 weeks.
+            """
+            if n_weeks == 4:
+                return (DATA['box_height'] + 12) * (n_weeks + 1) + 1            
+            if n_weeks == 5:
+                return (DATA['box_height'] + 10) * (n_weeks + 1) + 1
+            if n_weeks == 6:
+                return (DATA['box_height'] + 8) * (n_weeks + 1) + 5
+            raise Exception(f'Invalid n_weeks: {n_weeks}')
+
+        n_weeks = len(self.month_cal)
+
+        # Vertical lines
+        y_top = 149
+        y_bottom = calc_y_bottom()
+
+        for x in range(10, 1050, 145):
+            self.draw.line((x, y_top, x, y_bottom), width=3, fill ="black")
+
+        # Horizontal lines
+        x_left = 10
+        x_right = 1023
+        for y in range(150, 800, 90):
+            self.draw.line((x_left, y, x_right, y), width=3, fill ="black")
+            n_weeks -= 1
+            if n_weeks < 0:
+                break
+
+
+    def draw_title_page(self, year, title_text):
+        """
+        Draw the calendar title page showing the year.
+        """
+        W, H = (self.im.width, self.im.height)
+        _, _, w, h = self.draw.textbbox((0, 0), str(year), font=DATA["huge_font"])
+        self.draw.text(((W-w)/2, (H-h)/3), str(year), font=DATA['huge_font'], fill='blue')
+
+        _, _, w, h = self.draw.textbbox((0, 0), title_text, font=DATA["title_font"])
+        self.draw.text(((W-w)/2, (H-h)/3*2), title_text, font=DATA['title_font'], fill='green')
+
+        self.im.save(f'title.jpg', "JPEG", optimize=True, quality=80)
+
+
+class MakeCalendar():
+    """
+    Make the whole wall calendar.
+    """
+    def __init__(self, args):
+        self.args = args
+        self.load_json()
+
+        root = tk.Tk()
+        self.screen_width = root.winfo_screenwidth()
+        self.screen_height = root.winfo_screenheight()
+        root.destroy()
+
+
+    def load_json(self):
+        with open(self.args.input_filename) as jin:
+            self.json = json.load(jin)
+
+    def resize(self, image_path):
+        """
+        Resize an image to fit in the drawing image - self.im.
+        """
+        image = Image.open(image_path)
+        scale = min(DATA['root_width'] / image.width,
+                    DATA['root_height'] / image.height)
+        resized = image.resize((round(scale * image.width),
+                                round(scale * image.height)), Image.LANCZOS)
+        im = Image.new('RGB', (DATA['root_width'], DATA['root_height']), (256, 256, 256))
+        position = (int((im.width - resized.width) / 2),
+                    int((im.height - resized.height) / 2))
+        im.paste(resized, position)
+        im.save(image_path.name, "JPEG", optimize=True, quality=80)
+
+
+    def render(self):
+        """
+        Run RenderMonth for each month and prepare the images.
+        """
+        for month in range(1, 13):
+            render = RenderMonth(self.args, self.args.year, month)
+            render.draw_calendar()
+            render.save()
+        render = RenderMonth(self.args, self.args.year, 1)
+        # draw_title_page also saves the JPG.
+        render.draw_title_page(self.args.year, self.json['title_text'])
+
+        # debug: make sure all of the image files are there.
+        self.images = []
+        for month in range(1, 13):
+            month_name = calendar.month_name[month]
+            image_path = Path(self.json['directory']) / self.json['images'][month]
+            self.resize(image_path)
+            self.images.append(image_path.name)
+            print(image_path.name, f'{Path(image_path.name).exists()}')
+        # TODO
+        # collect the title.jpg file, mohth.jpg files and the
+        # scaled image files and call
+        #   magick names str(args.year).pdf
+        # Look at convert, mv, slide_show or video_clip in video_tools
+        cmd = ['magick', 'title.jpg'] + \
+              list(
+                  list(
+	              itertools.chain.from_iterable(
+		          zip([str(image).replace('\\', '/') for image in self.images],
+                              [f'{month}.jpg' for month in calendar.month_name[1:]])))) + \
+              [f'{self.args.year}.pdf']
+
+        print(cmd)
+        ret = subprocess.call(cmd)
+        if ret != 0:
+            raise RuntimeError(f'Got a subprocess.call error {ret}, command={" ".join(cmd)}')
+
+
+def main(args):
+    """
+    Starting point.
+    """
+    make_calendar = MakeCalendar(args)
+    make_calendar.render()
+
+if __name__ == '__main__':
+    sys.exit(main(build_parser().parse_args()))
